f5600d06b9321b17691169df8076849d1f7729d9
Alisa Alexandrova <alisaalexandrova2001@gmail.com>
2022-11-29 12:41:23 +0200

    ДЗ телефонный справочник, коллекция

---
+++ src/Fullname.java
@@ -0,0 +1,37 @@
+import java.util.Objects;
+
+public class Fullname {
+    private final String firstName;
+    private final String lastName;
+
+    public Fullname(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fullname fullname = (Fullname) o;
+        return Objects.equals(firstName, fullname.firstName) && Objects.equals(lastName, fullname.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
 public class Main {
+    private final static Random RANDOM = new Random();
+
     public static void main(String[] args) {
 
+        //Задание 1
+        PhoneBook telephones = new PhoneBook();
+        while (telephones.getEntries().size() < 20) {
+            telephones.addPhone(getRandomName(), getRandomPhone());
+        }
+
+        for (Map.Entry<Fullname, String> entry : telephones.getEntries()) {
+            System.out.printf("%s- %s%n", entry.getKey(), entry.getValue());
+        }
+
+    }
+
+    public static Fullname getRandomName() {
+        String firstName = "Name" + RANDOM.nextInt(1000);
+        String lastName = "Lastname" + RANDOM.nextInt(1000);
+        return new Fullname(firstName, lastName);
+    }
+
+    public static String getRandomPhone() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 11; i++) {
+            stringBuilder.append(RANDOM.nextInt(10));
+        }
+        return stringBuilder.toString();
     }
 }
